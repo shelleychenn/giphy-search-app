327ed424ee2d441098a31fc0f86f6df712fd9154
shelleychenn <shelleychen322@gmail.com>
2020-08-18 16:16:25 -0400

    Create and initialize redux store

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import Display from './components/Display';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Display />
+    </div>
+  );
 }
 
 export default App;
