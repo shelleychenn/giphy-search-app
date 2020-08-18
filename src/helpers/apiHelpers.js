46835df979a2f8691713fc24c0ce254daff2ef24
shelleychenn <shelleychen322@gmail.com>
2020-08-18 17:50:42 -0400

    Update getGifs api helper

--- src/helpers/apiHelpers.js
+++ src/helpers/apiHelpers.js
@@ -1,11 +1,18 @@
 import axios from 'axios';
 import API_KEY from './API_KEY';
+import searchGifs from '../actions';
 
-const API_URL = 'api.giphy.com/v1/gifs/search';
+const API_URL = 'https://api.giphy.com/v1/gifs/search';
 
 const apiHelpers = {
   getGifs: (queryTerm) => {
-    return axios.get(`${API_URL}?api_key=${API_KEY}&q=${queryTerm}`);
+    return (dispatch) => {
+      return axios
+        .get(`${API_URL}?api_key=${API_KEY}&q=${queryTerm}`)
+        .then(({ data }) => {
+          dispatch(searchGifs(data.data));
+        });
+    };
   },
 };
 
